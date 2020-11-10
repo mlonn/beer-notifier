329455321577431657edcbb0f4988dc1d73348ba
Simon Arneson <simon@pelag.io>
2020-11-10 20:16:35 +0100

    Reset cron

--- components/beer/beer_reminders.js
+++ components/beer/beer_reminders.js
@@ -25,7 +25,7 @@ module.exports = controller => {
     });
   });
 
-  const dailyUpdate = schedule.scheduleJob("15 * * * *", err => {
+  const dailyUpdate = schedule.scheduleJob("0 12 * * *", err => {
     if (err) {
       console.log(err);
     }
